2492e8d0f0f802f7c743f2f886c9ff9aa8ced8be
lgtonga <haianyidong@sina.com>
2016-11-23 13:48:36 +0800

    r3we

--- src/main/java/com/test/soft/tlg/hello/App.java
+++ src/main/java/com/test/soft/tlg/hello/App.java
@@ -1,13 +1,13 @@
 package com.test.soft.tlg.hello;
 
 /**
- * Hello world!
+ * tlkdsjfkasdljfka
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello ewrwe!" );
     }
 }
